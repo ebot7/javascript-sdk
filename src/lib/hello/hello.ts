1a4fe88a749937c8be9c34c0198ab060b9254d27
Fua <fua.tse.extern@e-bot7.com>
2021-04-27 09:29:38 +0100

    Added explicit return type and type doc for the greet function.

--- src/lib/hello/hello.ts
+++ src/lib/hello/hello.ts
@@ -16,7 +16,8 @@
  * ```
  * @param name - the name(default=Ebot) to use for the greeting
  * @param lang - the language to use in greeting - en(default) or es
+ * @returns The greeting text based on language and name passed.
  */
-export const greet = (name: string, lang: string) => {
+export const greet = (name: string, lang: string): string => {
   return lang?.toLowerCase() == 'es' ? `Hola! ${name}` : `Hello! ${name}`;
 };
