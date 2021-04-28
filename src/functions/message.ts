887e419007e4e48de956fadafe6051fd97ead064
Fua <fua.tse.extern@e-bot7.com>
2021-04-28 23:05:32 +0100

    WIP: Modified convId to conversationId for better meaning

--- src/functions/message.ts
+++ src/functions/message.ts
@@ -3,13 +3,13 @@ import { Client } from '../client';
 export interface ICreateMessageOptions {
   client: Client;
   body: string;
-  convId: string;
+  conversationId: string;
   source: string;
   botId: string;
 }
 
 export async function createMessage(options: ICreateMessageOptions) {
-  const url = `v1/bots/${options.botId}/convs/${options.convId}/messages`;
+  const url = `v1/bots/${options.botId}/convs/${options.conversationId}/messages`;
   const result = await options.client.getInstance().post(url, {
     body: options.body,
     source: options.source,
