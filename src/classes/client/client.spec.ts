0243dc230b78e15cd46fdccafd07288f4be12da0
Fua <fua.tse.extern@e-bot7.com>
2021-05-03 10:53:30 +0100

    Adds client service class unit test case.

---
+++ src/classes/client/client.spec.ts
@@ -0,0 +1,23 @@
+import config from '../../config/config.json';
+
+import { Client } from './client';
+
+// Default parameters to be assigned if present in config.json
+const client = new Client();
+
+describe('test Client class, methods and proper axios configuration', () => {
+  it('should be an instance of a Client class', () => {
+    expect(client).toBeInstanceOf(Client);
+  });
+  it('should have a getInstance method that always returns an AxiosInstance object', () => {
+    expect(client.getInstance()).toBeInstanceOf(Object);
+  });
+  it('should have an axios instance that returns correct baseURL', () => {
+    expect(client.getInstance().defaults.baseURL).toEqual(config?.baseURL);
+  });
+  it('should have an axios instance that returns correct header bearer authorization', () => {
+    expect(client.getInstance()?.defaults?.headers?.['Authorization']).toEqual(
+      `Bearer ${config?.bearerToken}`
+    );
+  });
+});
