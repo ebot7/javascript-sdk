def2e3a3958e78b26c729e2ec7293bc269f477ca
Fua <fua.tse.extern@e-bot7.com>
2021-05-03 22:06:57 +0100

    Adds unit test cases for all method exposed by the external conversation api client.

---
+++ src/classes/external-conversation/external-conversation.spec.ts
@@ -0,0 +1,53 @@
+import { Client } from '../client';
+
+import { ExternalConversation } from './external-conversation';
+
+const client = new Client();
+const conversation = new ExternalConversation(client);
+
+describe('test External Conversation client class', () => {
+  describe('test External Conversation instance created successfully', () => {
+    it('should be an instance of a External Conversation class', () => {
+      expect(conversation instanceof ExternalConversation).toBe(true);
+    });
+  });
+
+  describe('test external conversation get api method', () => {
+    const botId = 'myTestBotId';
+    const externalId = 'myTestExtCovId';
+    const mock = jest.spyOn(client.getInstance(), 'get');
+    mock.mockReturnValueOnce(Promise.resolve({ data: {} }));
+
+    it('should be called with correct path and parameters', async () => {
+      const params = {
+        botId: botId,
+        externalId: externalId,
+      };
+      await conversation.get(params);
+      expect(mock).toBeCalledWith(
+        `v1/bots/${botId}/external-convs/${externalId}`
+      );
+    });
+  });
+
+  describe('test external conversation patch api method', () => {
+    const botId = 'myTestBotId';
+    const externalId = 'myTestExtCovId';
+    const mock = jest.spyOn(client.getInstance(), 'patch');
+    mock.mockReturnValueOnce(Promise.resolve({ data: {} }));
+
+    it('should be called with correct path and parameters', async () => {
+      const params = {
+        body: 'some well formatted body json',
+      };
+      await conversation.patch({
+        ...params,
+        ...{ botId: botId, externalId: externalId },
+      });
+      expect(mock).toBeCalledWith(
+        `v1/bots/${botId}/external-convs/${externalId}`,
+        params
+      );
+    });
+  });
+});
