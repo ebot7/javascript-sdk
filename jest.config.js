1e0411626bffb6b71872eb96561269ae2b69a6a5
Fua <fua.tse.extern@e-bot7.com>
2021-05-03 09:48:16 +0100

    Switched test module from ava to jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+
+const {defaults} = require('jest-config');
+module.exports = { 
+  displayName: {
+    name: 'ebot7-JS-SDK',
+    color: 'blue',
+  },
+  testTimeout: 15000,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  "testPathIgnorePatterns" : [
+    "/node_modules/",
+    //"<rootDir>/build/module/",
+    "<rootDir>/build/"  
+  ]
+};
+
