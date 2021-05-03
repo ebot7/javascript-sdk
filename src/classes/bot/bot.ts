96594e89cc04d5f75438c3ec86d04c82fc0f9514
Fua <fua.tse.extern@e-bot7.com>
2021-05-03 11:17:09 +0100

    Adds bot service class that exposes methods to list bots and get bot by botId

---
+++ src/classes/bot/bot.ts
@@ -0,0 +1,55 @@
+import { Client } from '../client';
+
+export interface IListBotOptions {
+  offset?: number;
+  limit?: number;
+}
+
+export interface IGetBotOptions {
+  botId: string;
+}
+
+export type BotType = {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  orgId: string;
+};
+
+export class Bot {
+  constructor(private readonly client: Client) {}
+
+  /**
+   * Fetches list of botIds.
+   *
+   * @param {IListBotOptions} options - Instance of IListBotOptions
+   * @returns {Promise<Array<string>>} List of bot Ids
+   * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   */
+  async list(options: IListBotOptions = {}): Promise<Array<string>> {
+    const url = 'v1/bots/';
+    const result = await this.client.getInstance().get(url, {
+      params: {
+        offset: options?.offset,
+        limit: options?.limit,
+      },
+    });
+
+    return result.data.items;
+  }
+
+  /**
+   * Gets bot object for supplied botId
+   *
+   * @param {IGetBotOptions} options - Instance of IGetBotOptions
+   * @returns {Promise<Bot>} Single bot object
+   * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   */
+  async get(options: IGetBotOptions): Promise<BotType> {
+    const url = `v1/bots/${options.botId}`;
+    const result = await this.client.getInstance().get(url);
+
+    return result.data.item;
+  }
+}
