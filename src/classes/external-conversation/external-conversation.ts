35dd0fac4d85fc0fc9491e907984b0bd97a76cc3
Fua <fua.tse.extern@e-bot7.com>
2021-05-03 12:33:23 +0100

    Adds external conversation client class that exposes methods toget an external conversation and to patch an external conversation

---
+++ src/classes/external-conversation/external-conversation.ts
@@ -0,0 +1,50 @@
+import { Client } from '../client';
+import { ConversationType } from '../conversation/conversation';
+
+export interface IGetExternalConversationOptions {
+  botId: string;
+  externalId: string; //The unique ID of the conversation in the external platform
+}
+
+export interface IUpdateExternalConversationOptions
+  extends IGetExternalConversationOptions {
+  body: string | unknown;
+}
+
+export class ExternalConversation {
+  constructor(private readonly client: Client) {}
+
+  /**
+   *  Gets an active conversation for bot with botId by its externalId
+   *
+   * @param {IGetExternalConversationOptions} options - Instance of IGetExternalConversationOptions
+   * @returns {Promise<ConversationType>} Single conversation object
+   * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   */
+  async get(
+    options: IGetExternalConversationOptions
+  ): Promise<ConversationType> {
+    const url = `/v1/bots/${options.botId}/external-convs/${options.externalId}`;
+    const result = await this.client.getInstance().get(url);
+
+    return result.data.item;
+  }
+
+  /**
+   * Updates an existing conversation by its botId and externalId
+   *
+   * @param {IUpdateExternalConversationOptions} options - Instance of IUpdateExternalConversationOptions
+   * @returns {Promise<ConversationType>} Single conversation object
+   * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   */
+  async patch(
+    options: IUpdateExternalConversationOptions
+  ): Promise<ConversationType> {
+    const url = `/v1/bots/${options.botId}/external-convs/${options.externalId}`;
+    const result = await this.client.getInstance().patch(url, {
+      body: options.body,
+    });
+
+    return result.data.item;
+  }
+}
