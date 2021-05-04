f984c9166ae3517bf8765a5c9ca8d15feba05a87
Fua <fua.tse.extern@e-bot7.com>
2021-05-04 23:43:54 +0100

    First attempt with openapi for the js sdk

--- src/classes/bot/bot.ts
+++ src/classes/bot/bot.ts
@@ -26,6 +26,49 @@ export class Bot {
    * @param {IListBotOptions} options - Instance of IListBotOptions
    * @returns {Promise<Array<string>>} List of bot Ids
    * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   *
+   * @openapi
+   * /bots:
+   *   get:
+   *     operationId: findManyBots
+   *     summary: List bots
+   *     description: Get a list of bot ids for which the current application which is enabled.
+   *     produces:
+   *     - "application/json"
+   *     consumes:
+   *     - "application/json"
+   *     parameters:
+   *       - description: The maximum number of records to return.
+   *         required: false
+   *         name: limit
+   *         in: query
+   *         schema:
+   *           type: number
+   *       - description: The offset of the first records to return.
+   *         required: false
+   *         name: offset
+   *         in: query
+   *         schema:
+   *           type: number
+   *     responses:
+   *       '200':
+   *         description: A list of available bots
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/BotEngineBotListApiOutput'
+   *       '401':
+   *         description: Unauthorized.
+   *       '403':
+   *         description: Forbidden.
+   *       '404':
+   *         description: Not Found.
+   *       '408':
+   *         description: Request Timeout.
+   *       '500':
+   *         description: Internal Server Error.
+   *     tags:
+   *       - Bots
    */
   async list(options: IListBotOptions = {}): Promise<Array<string>> {
     const url = '/bots/';
@@ -45,6 +88,10 @@ export class Bot {
    * @param {IGetBotOptions} options - Instance of IGetBotOptions
    * @returns {Promise<Bot>} Single bot object
    * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   *
+   * @openapi
+   *
+   *
    */
   async get(options: IGetBotOptions): Promise<BotType> {
     const url = `/bots/${options.botId}`;
