80f3f336b7cc5455125621f9fa303182c1e8d934
Fua <fua.tse.extern@e-bot7.com>
2021-05-19 16:37:01 +0100

    WIP: JS SDK Swagger Doc AutoGen

--- src/lib/classes/bots/bots.client.ts
+++ src/lib/classes/bots/bots.client.ts
@@ -1,3 +1,5 @@
+import { Errors, GET, Path } from 'typescript-rest';
+import { Consumes, Produces, Response, Tags } from 'typescript-rest-swagger';
 import { Ebot7Client, IEbot7ClientRequestPaging } from '../client';
 import {
   IEbot7BotListOutput,
@@ -5,16 +7,46 @@ import {
   IEbot7GetBotOptions,
 } from './bots.interfaces';
 
+@Consumes('application/json')
+@Produces('application/json')
+@Tags('bot', 'bots', 'ebot7', 'sdk')
 export class Ebot7BotClient {
   constructor(private readonly client: Ebot7Client) {}
 
   /**
    * Fetches list of botIds.
    *
-   * @param paging
-   * @returns List of bot Ids
+   * @param {IEbot7ClientRequestPaging} paging
+   * @returns {Promise<IEbot7BotListOutput>} List of bot Ids
    * @throws {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
    */
+
+  @GET
+  @Path('bots/')
+  @Response<IEbot7BotListOutput>(
+    200,
+    'A json object containing property items which is a list of bot Ids.'
+  )
+  @Response<Errors.BadRequestError>(
+    400,
+    'The request could not be understood by the server due to malformed syntax'
+  )
+  @Response<Errors.UnauthorizedError>(
+    401,
+    'The request requires user authentication.'
+  )
+  @Response<Errors.ForbiddenError>(
+    403,
+    'The server understood the request, but is refusing to fullfil it.'
+  )
+  @Response<Errors.NotFoundError>(
+    404,
+    'The server has not found anything matching the Request-URI.'
+  )
+  @Response<Errors.InternalServerError>(
+    500,
+    'The server encountered an unexpected condition or error.'
+  )
   public async findMany(
     paging: IEbot7ClientRequestPaging = {}
   ): Promise<IEbot7BotListOutput> {
@@ -27,10 +59,37 @@ export class Ebot7BotClient {
   /**
    * Gets bot object for supplied botId
    *
-   * @param options - Instance of IGetBotOptions
-   * @returns Single bot object
+   * @param {IEbot7GetBotOptions} options - Instance of IGetBotOptions
+   * @returns {Promise<IEbot7BotOutput>} Single bot object output
    * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
    */
+
+  @GET
+  @Path('bots/:botId')
+  @Response<IEbot7BotOutput>(
+    200,
+    'A json object containing property item which is a bot object.'
+  )
+  @Response<Errors.BadRequestError>(
+    400,
+    'The request could not be understood by the server due to malformed syntax'
+  )
+  @Response<Errors.UnauthorizedError>(
+    401,
+    'The request requires user authentication.'
+  )
+  @Response<Errors.ForbiddenError>(
+    403,
+    'The server understood the request, but is refusing to fullfil it.'
+  )
+  @Response<Errors.NotFoundError>(
+    404,
+    'The server has not found anything matching the Request-URI.'
+  )
+  @Response<Errors.InternalServerError>(
+    500,
+    'The server encountered an unexpected condition or error.'
+  )
   async findOne(options: IEbot7GetBotOptions): Promise<IEbot7BotOutput> {
     const url = `bots/${options.botId}`;
     const result = await this.client.get(url);
--- src/lib/classes/external-conversations/external-conversations.client.ts
+++ src/lib/classes/external-conversations/external-conversations.client.ts
@@ -1,3 +1,5 @@
+import { Errors, GET, PATCH, Path } from 'typescript-rest';
+import { Consumes, Produces, Response, Tags } from 'typescript-rest-swagger';
 import {
   IEbot7GetExternalConversationOptions,
   IUpdateExternalConversationOptions,
@@ -5,16 +7,55 @@ import {
 import { Ebot7Client } from '../client';
 import { IEbot7ConversationOutput } from '../conversations';
 
+@Consumes('application/json')
+@Produces('application/json')
+@Tags(
+  'bot',
+  'bots',
+  'ebot7',
+  'sdk',
+  'conv',
+  'conversations',
+  'external',
+  'external-conversation'
+)
 export class Ebot7ExternalConversationClient {
   constructor(private readonly client: Ebot7Client) {}
 
   /**
    *  Gets an external conversation for bot with botId by its externalId
    *
-   * @param  options
-   * @returns Single conversation object
+   * @param  {IEbot7GetExternalConversationOptions} options - Instance of IEbot7GetExternalConversationOptions
+   * @returns {Promise<IEbot7ConversationOutput>} A json object containing property item which is an external conversation object.
    * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
    */
+
+  @GET
+  @Path('bots/:botId/external-convs/:externalId')
+  @Response<IEbot7ConversationOutput>(
+    200,
+    'A json object containing property item which is an external conversation object.'
+  )
+  @Response<Errors.BadRequestError>(
+    400,
+    'The request could not be understood by the server due to malformed syntax'
+  )
+  @Response<Errors.UnauthorizedError>(
+    401,
+    'The request requires user authentication.'
+  )
+  @Response<Errors.ForbiddenError>(
+    403,
+    'The server understood the request, but is refusing to fullfil it.'
+  )
+  @Response<Errors.NotFoundError>(
+    404,
+    'The server has not found anything matching the Request-URI.'
+  )
+  @Response<Errors.InternalServerError>(
+    500,
+    'The server encountered an unexpected condition or error.'
+  )
   async findOne(
     options: IEbot7GetExternalConversationOptions
   ): Promise<IEbot7ConversationOutput> {
@@ -27,10 +68,37 @@ export class Ebot7ExternalConversationClient {
   /**
    * Updates an existing conversation by its botId and externalId
    *
-   * @param options
-   * @returns  Single conversation object
+   * @param {IUpdateExternalConversationOptions} options - Instance of IUpdateExternalConversationOptions
+   * @returns  {Promise<IEbot7ConversationOutput>} A json object containing property item which is a conversation object.
    * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
    */
+
+  @PATCH
+  @Path('bots/:botId/external-convs/:externalId')
+  @Response<IEbot7ConversationOutput>(
+    200,
+    'A json object containing property item which is an external conversation object.'
+  )
+  @Response<Errors.BadRequestError>(
+    400,
+    'The request could not be understood by the server due to malformed syntax'
+  )
+  @Response<Errors.UnauthorizedError>(
+    401,
+    'The request requires user authentication.'
+  )
+  @Response<Errors.ForbiddenError>(
+    403,
+    'The server understood the request, but is refusing to fullfil it.'
+  )
+  @Response<Errors.NotFoundError>(
+    404,
+    'The server has not found anything matching the Request-URI.'
+  )
+  @Response<Errors.InternalServerError>(
+    500,
+    'The server encountered an unexpected condition or error.'
+  )
   async patch(
     options: IUpdateExternalConversationOptions
   ): Promise<IEbot7ConversationOutput> {
