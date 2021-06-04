aee9f1bbc3e057693f3a2a183afd22d68ff56c33
Fua <fua.tse.extern@e-bot7.com>
2021-06-04 16:56:44 +0100

    Adds app client class with install method for installing app instance. Also impleaments unit test for the method

---
+++ src/lib/classes/app/app.client.ts
@@ -0,0 +1,24 @@
+import {
+  IEbot7InstallApplicationInstanceOptions,
+  IEbot7InstallApplicationInstanceOutput,
+} from '.';
+import { Ebot7Client } from '../client';
+
+export class Ebot7AppClient {
+  constructor(private readonly client: Ebot7Client) {}
+
+  /**
+   * Installs a new application instance and returns the application instance and instance key.
+   *
+   * @param  options
+   * @returns {Promise<IEbot7InstallApplicationInstanceOutput>} An object that contains the application instance and application instance key
+   * @throws  {Not Found | Unauthorized | Forbidden | Request Timeout | Internal Server Error | EAI_AGAIN} HTTP Error
+   */
+  public async install(
+    options: IEbot7InstallApplicationInstanceOptions
+  ): Promise<IEbot7InstallApplicationInstanceOutput> {
+    const url = 'application/install';
+    const result = await this.client.post(url, options);
+    return result.data;
+  }
+}
